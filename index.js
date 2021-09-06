789dd24db07957635b885923fcf2393c174f9a76
eldtr <89798787+eldtr@users.noreply.github.com>
2021-09-06 11:53:04 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+      function getIMC(){
+        var peso=parseFloat((document.getElementById("peso");
+                             var altura=parseFloat((document.getElementById("altura");
+                            var IMC=peso/(altura*altura);
+        document.getElementById("IMC").innerHTML=IMC;
+      }
+      
